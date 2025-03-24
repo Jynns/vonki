d306d13db67a394f23ef828c32f830ea20b982fe
jannisarnold <jannisarnold1@gmail.com>
2025-03-24 17:43:25 +0100

    added Readme and routes

---
+++ README.md
@@ -0,0 +1,7 @@
+## Enter venv
+source venv/bin/activate
+
+## Run flask
+enter venv
+command: export FLASK_APP=vonki.py
+command: flask run
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World"
+    user = {'username': 'TestUser'}
+    return render_template('index.html', user = user, title = 'Home')
